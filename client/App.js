8588ed4017b42a2e48bc8541ba749b2bef2458ef
Michael <michael_kaminski@outlook.com>
2023-05-11 00:30:12 -0500

    more init

--- client/App.js
+++ client/App.js
@@ -1,14 +1,39 @@
 import React from 'react'
 // 1. import `NativeBaseProvider` component
-import { NativeBaseProvider, Text, Box } from 'native-base'
+import { NativeBaseProvider, Text, Box, Button, View } from 'native-base'
+import { NavigationContainer } from '@react-navigation/native'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+
+const Stack = createNativeStackNavigator()
 
 export default function App() {
   // 2. Use at the root of your app
   return (
-    <NativeBaseProvider>
-      <Box flex={1} bg='#fff' alignItems='center' justifyContent='center'>
-        <Text>Open up App.js to start working on your app!</Text>
-      </Box>
-    </NativeBaseProvider>
+    <NavigationContainer>
+      <NativeBaseProvider>
+        <Stack.Navigator>
+          <Stack.Screen name='Home' component={HomeScreen} />
+          <Stack.Screen name='Login' component={LoginScreen} />
+          <Stack.Screen name='Register' component={RegisterScreen} />
+        </Stack.Navigator>
+      </NativeBaseProvider>
+    </NavigationContainer>
   )
+}
+
+const HomeScreen = ({ navigation }) => {
+  return (
+    <View>
+      <Text>Hello This is home page</Text>
+      <Button title='Home' />
+    </View>
+  )
+}
+
+const LoginScreen = ({ navigation }) => {
+  return <Button title='Login' />
+}
+
+const RegisterScreen = ({ navigation }) => {
+  return <Button title='Register Page' />
 }
