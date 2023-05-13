4bc5d48a3d47204add673f8388f760d282cdf279
Michael <michael_kaminski@outlook.com>
2023-05-13 16:06:28 -0500

    can dynamically add workouts (no db integration)

--- client/screens/Workout.js
+++ client/screens/Workout.js
@@ -1,17 +1,53 @@
-import React from 'react'
-import { Input, Text, View } from 'native-base'
+import React, { useState } from 'react'
+import { Button, Input, Text, View } from 'native-base'
+
+// Need to change it so that I can add reps to individual sets
+// Maybe do an add set button that adds the current reps x set
+// Can add multiple sets with the same rep range or individual
+// with custom
 
 export const Workout = ({ workoutData }) => {
+  const [exercises, setExercises] = useState([{ name: '', reps: '', sets: '' }])
+
+  const addExercise = () => {
+    setExercises([...exercises, { name: '', reps: '', sets: '' }])
+  }
+
+  const updateExercise = (index, field, value) => {
+    const updatedExercises = [...exercises]
+    updatedExercises[index][field] = value
+    setExercises(updatedExercises)
+  }
+
   return (
     <View>
       <View>
         <Text>Build your workout.</Text>
       </View>
       <View>
-        <View>
-          <Text>Exercise #1</Text>
-          <Input />
-        </View>
+        {exercises.map((exercise, index) => (
+          <View key={index}>
+            <Text>Exercise #{index + 1}</Text>
+            <Input
+              placeholder='Exercise name'
+              value={exercise.name}
+              onChangeText={(value) => updateExercise(index, 'name', value)}
+            />
+            <Input
+              placeholder='Sets'
+              value={exercise.sets}
+              onChangeText={(value) => updateExercise(index, 'sets', value)}
+            />
+            <Input
+              placeholder='Reps'
+              value={exercise.reps}
+              onChangeText={(value) => updateExercise(index, 'reps', value)}
+            />
+          </View>
+        ))}
+        <Button onPress={addExercise}>
+          <Text>Add exercise</Text>
+        </Button>
       </View>
     </View>
   )
