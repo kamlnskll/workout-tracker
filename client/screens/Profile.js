af213dfb1a966a62e8cf8114f638030792a0ebb1
Michael <michael_kaminski@outlook.com>
2023-05-13 14:25:01 -0500

    added basic tab navigation between screens

---
+++ client/screens/Profile.js
@@ -0,0 +1,12 @@
+import { Text, View } from 'native-base'
+import React from 'react'
+
+const Profile = ({ navigation }) => {
+  return (
+    <View>
+      <Text>Workouts</Text>
+    </View>
+  )
+}
+
+export default Profile
