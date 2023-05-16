74296a4291500580a49cd279de2cb4a2f2564b25
Michael <michael_kaminski@outlook.com>
2023-05-16 17:28:21 -0500

    workouts are fetched on backend, not frontend

--- client/components/WorkoutCard.js
+++ client/components/WorkoutCard.js
@@ -1,15 +1,20 @@
-import { View } from 'native-base'
+import { Text, View } from 'native-base'
 import React from 'react'
 
 // Shows a preview of each workout in a clickable UI form that will navigate them to the workout data.
 
 export const WorkoutCard = ({ workoutData }) => {
+  const exercises = workoutData.exercises
+
   return (
     <View>
-      <Text>
-        This text is in a workout card, and will be used to show workouts in a
-        card format.
-      </Text>
+      {exercises?.map((exercise) => (
+        <View>
+          <Text>{exercise.key}</Text>
+        </View>
+      ))}
+      <Text>This is a workout card and will contain all your workouts</Text>
+      <Text>{workoutData.uploaderID}</Text>
     </View>
   )
 }
--- client/screens/Home.js
+++ client/screens/Home.js
@@ -1,18 +1,48 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Text, Button, View } from 'native-base'
 import { StyleSheet } from 'react-native'
 import { WorkoutCard } from '../components/WorkoutCard'
 import { auth, database } from '../firebase/firebase'
+import { collection, query, where, getDocs } from 'firebase/firestore'
 
 export const Home = ({ navigation }) => {
   const [workouts, setWorkouts] = useState([])
-  const user = auth.currentUser
-  const currentUserID = user.uid
+  const currentUserID = auth.currentUser.uid
+
+  const getWorkoutsFromDB = async () => {
+    // Create query for a certain condition aka for workouts created by the user and get the documents
+
+    const dbQuery = query(
+      collection(database, 'workouts'),
+      where('uploaderID', '==', currentUserID)
+    )
+
+    const fetchData = await getDocs(dbQuery)
+    const fetchWorkouts = fetchData.docs
+    return fetchWorkouts
+  }
+
+  useEffect(() => {
+    getWorkoutsFromDB()
+      .then((res) => {
+        const response = res
+        if (response) {
+          const data = response
+          data.forEach((doc) => {
+            const docData = doc.data()
+            console.log(docData)
+            setWorkouts(docData.exercises)
+            // return docData
+          })
+        }
+      })
+      .catch((err) => console.log(err))
+  }, [])
 
   return (
     <View>
       <View>
-        <Text onPress={() => console.log(currentUserID)}>Workouts</Text>
+        <Text onPress={() => console.log(workouts)}>Workouts</Text>
       </View>
       <View>
         {workouts.map((workout) => (
