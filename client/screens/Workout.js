13773a19e4ce74885bf8bf7a6fb1b4bf355c8ede
Michael <michael_kaminski@outlook.com>
2023-05-16 01:50:16 -0500

    exercises appear in a swipeable list & can b remov

--- client/screens/Workout.js
+++ client/screens/Workout.js
@@ -1,9 +1,10 @@
 import React, { useEffect, useState } from 'react'
-import { Button, Input, Text, View, HStack, Flex } from 'native-base'
+import { Button, Input, Text, View, HStack, Flex, Pressable } from 'native-base'
 import dayjs from 'dayjs'
 import { collection, addDoc } from 'firebase/firestore'
 import { database } from '../firebase/firebase'
 import { SwipeListView } from 'react-native-swipe-list-view'
+import uuid from 'react-native-uuid'
 
 // Need to change it so that I can add reps to individual sets
 // Maybe do an add set button that adds the current reps x set
@@ -11,7 +12,9 @@ import { SwipeListView } from 'react-native-swipe-list-view'
 // with custom
 
 export const Workout = ({ workoutData }) => {
-  const [exercises, setExercises] = useState([{ name: '', reps: '', sets: '' }])
+  const [exercises, setExercises] = useState([
+    { key: uuid.v4(), name: '', reps: '', sets: '' },
+  ])
 
   const [error, setError] = useState('')
 
@@ -22,7 +25,10 @@ export const Workout = ({ workoutData }) => {
   }, [error])
 
   const addExercise = () => {
-    setExercises([...exercises, { name: '', reps: '', sets: '' }])
+    setExercises([
+      ...exercises,
+      { key: uuid.v4(), name: '', reps: '', sets: '' },
+    ])
   }
 
   const updateExercise = (index, field, value) => {
@@ -31,7 +37,54 @@ export const Workout = ({ workoutData }) => {
     setExercises(updatedExercises)
   }
 
+  const closeRow = (rowMap, rowKey) => {
+    if (rowMap[rowKey]) {
+      rowMap[rowKey].closeRow()
+    }
+  }
+
+  const deleteRow = (rowMap, rowKey) => {
+    closeRow(rowMap, rowKey)
+    const newData = [...exercises]
+    const prevIndex = exercises.findIndex((item) => item.key === rowKey)
+    newData.splice(prevIndex, 1)
+    setExercises(newData)
+  }
+
+  const renderHiddenItem = (data, rowMap) => (
+    <HStack flex={1} py='0' my='2'>
+      <Pressable
+        px={4}
+        ml='auto'
+        cursor='pointer'
+        bg='dark.500'
+        justifyContent='center'
+        onPress={() => closeRow(rowMap, data.item.key)}
+        _pressed={{
+          opacity: 0.5,
+        }}
+        h='16'
+      >
+        <Text fontSize='xs'>Close</Text>
+      </Pressable>
+      <Pressable
+        px={4}
+        cursor='pointer'
+        bg='red.500'
+        justifyContent='center'
+        onPress={() => deleteRow(rowMap, data.item.key)}
+        _pressed={{
+          opacity: 0.5,
+        }}
+      >
+        <Text fontSize='xs'>Delete</Text>
+      </Pressable>
+    </HStack>
+  )
+
   const saveWorkoutInDB = async () => {
+    // attach unique key property to each document
+
     // save workout to Firestore
     await addDoc(collection(database, 'workouts'), { exercises })
       .then((res) => console.log('New document created', res))
@@ -47,32 +100,37 @@ export const Workout = ({ workoutData }) => {
           <Text my='4' ml='8' fontSize='xl' fontWeight='bold'>
             {date}
           </Text>
+
           <Text color='red.600'>{error}</Text>
         </View>
         <View>
           <SwipeListView
+            rightOpenValue={-130}
+            previewRowKey={'0'}
+            previewOpenValue={-40}
+            previewOpenDelay={3000}
             data={exercises}
-            renderItem={(exercise, index) => (
-              <View key={index} mb='3'>
+            renderItem={(exercise, rowMap) => (
+              <View key={exercise.key} h='16' bg='gray.100' my='2'>
                 <Text ml='8' fontSize={'xs'} fontWeight={'semibold'}>
-                  Exercise #{index + 1}
+                  Exercise
                 </Text>
                 <HStack justifyContent='center' space={3}>
                   <Input
                     placeholder='Exercise name'
                     value={exercise.name}
-                    onChangeText={(value) =>
-                      updateExercise(index, 'name', value)
-                    }
+                    // onChangeText={(value) =>
+                    //   updateExercise(index, 'name', value)
+                    // }
                     h='35'
                     w='180'
                   />
                   <Input
                     placeholder='Sets'
                     value={exercise.sets}
-                    onChangeText={(value) =>
-                      updateExercise(index, 'sets', value)
-                    }
+                    // onChangeText={(value) =>
+                    //   updateExercise(index, 'sets', value)
+                    // }
                     h='35'
                     w='20'
                     // mr='4'
@@ -80,9 +138,9 @@ export const Workout = ({ workoutData }) => {
                   <Input
                     placeholder='Reps'
                     value={exercise.reps}
-                    onChangeText={(value) =>
-                      updateExercise(index, 'reps', value)
-                    }
+                    // onChangeText={(value) =>
+                    //   updateExercise(index, 'reps', value)
+                    // }
                     h='35'
                     w='20'
                     // mr='4'
@@ -90,40 +148,11 @@ export const Workout = ({ workoutData }) => {
                 </HStack>
               </View>
             )}
+            renderHiddenItem={renderHiddenItem}
+            disableRightSwipe={true}
           />
-          {exercises.map((exercise, index) => (
-            <View key={index} mb='3'>
-              <Text ml='8' fontSize={'xs'} fontWeight={'semibold'}>
-                Exercise #{index + 1}
-              </Text>
-              <HStack justifyContent='center' space={3}>
-                <Input
-                  placeholder='Exercise name'
-                  value={exercise.name}
-                  onChangeText={(value) => updateExercise(index, 'name', value)}
-                  h='35'
-                  w='180'
-                />
-                <Input
-                  placeholder='Sets'
-                  value={exercise.sets}
-                  onChangeText={(value) => updateExercise(index, 'sets', value)}
-                  h='35'
-                  w='20'
-                  // mr='4'
-                />
-                <Input
-                  placeholder='Reps'
-                  value={exercise.reps}
-                  onChangeText={(value) => updateExercise(index, 'reps', value)}
-                  h='35'
-                  w='20'
-                  // mr='4'
-                />
-              </HStack>
-            </View>
-          ))}
-          <Button onPress={addExercise} w='16' mt='4' mx='auto'>
+
+          <Button onPress={addExercise} w='16' mt='0' mx='auto'>
             <Text>+</Text>
           </Button>
         </View>
