16c5a875e6b71ede1e83f86c32b9a08cdae69338
Michael <michael_kaminski@outlook.com>
2023-05-17 23:29:12 -0500

    started work on profile page

--- client/screens/Profile.js
+++ client/screens/Profile.js
@@ -1,11 +1,34 @@
-import { Text, View } from 'native-base'
+import { HStack, Image, Text, VStack, View } from 'native-base'
+import { auth } from '../firebase/firebase'
 import React from 'react'
 
 const Profile = ({ navigation }) => {
+  const currentUser = auth.currentUser
+
   return (
-    <View>
+    <View
+      my='1'
+      bg='primary.50'
+      borderColor={'black'}
+      borderWidth={'0.5'}
+      rounded='lg'
+      w='75%'
+      ml='4'
+      mr='auto'
+      justifyContent='left'
+    >
       <View>
-        <Text>Workouts</Text>
+        <Image
+          source={{ uri: `${currentUser.photoURL}` }}
+          alt='Profile Picture of User'
+        />
+        <Text onPress={() => console.log(currentUser)}>Profile</Text>
+      </View>
+      <View>
+        <VStack>
+          <Text>{currentUser.email}</Text>
+          <HStack></HStack>
+        </VStack>
       </View>
     </View>
   )
