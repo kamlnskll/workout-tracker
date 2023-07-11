3c54b3ed6a7b537ca3cfed4e2722c1a6859fac6d
Michael <michael_kaminski@outlook.com>
2023-07-10 21:07:00 -0500

    '

--- client/components/WorkoutStats.js
+++ client/components/WorkoutStats.js
@@ -1,10 +1,11 @@
 import React from 'react'
-import { HStack, Text, VStack, View } from 'native-base'
+import { Text, View } from 'native-base'
 
 const WorkoutStats = ({ workoutData }) => {
   return (
     <View>
       <Text>Workout Stats</Text>
+      <Text></Text>
     </View>
   )
 }
