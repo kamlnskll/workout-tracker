779e74c73a8fca9221b9831f5e89a59483a121c4
kamlnskll <michael_kaminski@outlook.com>
2023-07-18 11:49:03 -0500

    added date range selector for stats

--- client/screens/Home.js
+++ client/screens/Home.js
@@ -1,11 +1,12 @@
 import React, { useEffect, useState } from 'react'
-import { View, Heading, Pressable, HStack, ScrollView, Button, Text } from 'native-base'
+import { View, Heading, Pressable, HStack, ScrollView, Button, Text, Select, CheckIcon } from 'native-base'
 import { WorkoutCard } from '../components/WorkoutCard'
 import { auth, database } from '../firebase/firebase'
 import { collection, query, where, getDocs } from 'firebase/firestore'
 
 export const Home = ({ navigation }) => {
   const [workouts, setWorkouts] = useState()
+  const [dateRange, setDateRange] = useState('7d')
   const currentUserID = auth.currentUser.uid
 
   const getWorkoutsFromDB = async () => {
@@ -68,9 +69,25 @@ export const Home = ({ navigation }) => {
               ))
             : null}
         </ScrollView>
-        <View mx='8' mt='6' mb='3'>
-          <Heading>Your Stats</Heading>
-          <Text># of Workouts This Week/Month</Text>
+        <View  mx='8' mt='6' mb='3'>
+        <HStack>
+          <Heading my='auto'>Your Stats</Heading>
+          <Select defaultValue={dateRange} selectedValue={dateRange} ml='auto' maxHeight='32px' minWidth="120" accessibilityLabel="Range" placeholder="Date Range" _selectedItem={{
+        bg: "tertiary.200",
+        endIcon: <CheckIcon size="5" />
+      }} mt={1} onValueChange={(value) => {setDateRange(value)}}>
+          <Select.Item label="7 Days" value="7d" />
+          <Select.Item label="14 Days" value="14d" />
+          <Select.Item label="30 Days" value="30d" />
+          {/* <Select.Item label="Last 90 Days" value="90d" />
+          <Select.Item label="Last 6 months" value="180d" />
+          <Select.Item label="Last 1 year" value="365d" /> 
+          Implement these date ranges later on.
+          */}
+        </Select>
+        </HStack>
+        <Text># of Workouts This Week/Month</Text>
+
         </View>
       </View>
     </>
